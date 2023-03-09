61905c0c0bb6dbe7e1f306cfc6deb27399e14e23
Matt Fogle <msfogle@worcesterk12.org>
2023-03-09 09:08:45 -0500

    Initial Commit

---
+++ sample/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  sample
+//
+//  Created by Matthew S. Fogle on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
