4e7d19d3b77f627aad4779555634b6f97d3a8863
Matt Fogle <msfogle@worcesterk12.org>
2023-03-09 09:36:58 -0500

    Scene Detail

--- sample/ContentView.swift
+++ sample/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello There!")
+            Text("Kenobi to General Grievous")
         }
         .padding()
         //Star Wars Fun
